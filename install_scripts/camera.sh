18f4a0f969da742af3c954510c0c70e99e77f983
Christian Laußmann <ch.laussmann@gmail.com>
2018-12-20 20:58:07 +0100

    bugfixes

--- install_scripts/camera.sh
+++ install_scripts/camera.sh
@@ -1,5 +1,6 @@
 read -p "Have you already installed your camera? (y/N)" b
-if [ "$b" != "y" ] then
+if [ "$b" != "y" ] 
+then
 	echo "Loading default cameramodule in /etc/modules"
 	echo "#default Camera" >> /etc/modules;
 	echo "bcm2835-v4l2" >> /etc/modules;
--- install_scripts/config.sh
+++ install_scripts/config.sh
@@ -10,7 +10,8 @@ echo "$token" > /etc/burglar_warner/remote/botToken
 echo "Now: The notifier.\nIt will always notify you if motion is detected."
 echo "You can now enter all chatIDs that you want to be notified at motion detection"
 read -p "Enter a chatID:" chatID
-while ["$chatID" != ''] do
+while [ "$chatID" != '' ] 
+do
 	echo "$chatID" >> /etc/burglar_warner/notifier/chatIDs
 	read -p "Enter next chatID (empty to finish):" chatID
 done
@@ -19,7 +20,8 @@ done
 echo "Finally: The remote-controller!"
 echo "Now enter telegram-usernames that should be authorized to control the burglar-warner via telegram"
 read -p "Enter a username:" username
-while ["$username" != ''] do
+while [ "$username" != '' ] 
+do
 	echo "$username" >> /etc/burglar_warner/remote/authorized
 	read -p "Enter next username (empty to finish):" username
 done
