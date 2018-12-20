6c202c5438368f3255fc2867c59ffb498f6d3937
Christian Laußmann <ch.laussmann@gmail.com>
2018-12-20 11:35:37 +0100

    add sudoers-config

---
+++ install_scripts/sudoers_config.sh
@@ -0,0 +1,10 @@
+echo "The service will need to run some commands like poweroff without password."
+read -p "Do you wish the installer to create a sudoers entry for the service?(y/N)" a
+	if [ "$a" = "y" ]
+	then
+		echo -e "\e[93mEditing sudoers file for motion\e[0m"
+		echo "#Added by Raspi-burglar-warner" >> /etc/sudoers
+		echo "#Motion may shutdown the PC:" >> /etc/sudoers
+		echo "motion ALL=NOPASSWD:/sbin/reboot" >> /etc/sudoers
+		echo "motion ALL=NOPASSWD:/sbin/poweroff" >> /etc/sudoers
+	fi
--- makefile
+++ makefile
@@ -1,15 +1,18 @@
 install: service_install notifier_install
 
-run: install
-
-run_on_startup: install
-
 motion_install:
 	apt-get update
 	apt-get install motion
 
 config:
 
-service_install: config
+service_install: config sudoers_entry
 
 notifier_install: config motion_install
+
+sudoers_entry:
+	bash install_scripts/sudoers_config.sh
+
+camera_install:
+
+directories:
