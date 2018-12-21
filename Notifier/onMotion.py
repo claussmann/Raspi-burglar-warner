dbc356e8c27af16a366fb597eba88d961c1bfed6
Christian Laußmann <ch.laussmann@gmail.com>
2018-12-21 13:39:00 +0100

    correct import statement

--- Notifier/onMotion.py
+++ Notifier/onMotion.py
@@ -1,8 +1,10 @@
+import subprocess
+
 botToken = open("botToken", "r").read()
 botToken = botToken.replace('\n','')
 
 chatIDs = open("chatIDs", "r").read()
-chatIDs = users.split('\n')
+chatIDs = chatIDs.split('\n')
 
 for chatID in chatIDs:
 	url = "https://api.telegram.org/bot" + botToken + "/sendMessage?chat_id=" + str(chatID) + "&text=I%20think%20there%20is%20somethig..."
