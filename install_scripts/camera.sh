951ec528e6ace8cd543fceaf76142f537d40abdb
Christian Laußmann <ch.laussmann@gmail.com>
2018-12-22 17:20:18 +0100

    Add warning for not oficially supported cameras

--- install_scripts/camera.sh
+++ install_scripts/camera.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-read -p "Have you already installed your camera? (y/N)" b
-if [ "$b" != "y" ] 
+read -p "Have you already configured your camera? (y/N)" a
+if [ "$a" != "y" ]
 then
-	echo "Loading default cameramodule in /etc/modules"
-	echo "#default Camera" >> /etc/modules;
-	echo "bcm2835-v4l2" >> /etc/modules;
+	read -p "Do you want to configure an official Raspi-Camera? (Y/n)" b
+	if [ "$b" != "n" ]
+	then
+		echo "Loading default cameramodule in /etc/modules"
+		echo "#default Camera" >> /etc/modules;
+		echo "bcm2835-v4l2" >> /etc/modules;
+	else
+		echo "\e[1;91mCannot configure your camera. Please load the modules on your own.\e[0m"
+	fi
 fi
